75386aa9f0a9b2a84ec7b092cdcd8a65166b548f
Yasuaki Uechi <y@uechi.io>
2019-06-28 11:15:49 +0900

    feat: mathjax3 support

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 const express = require('express')
 const memCache = require('memory-cache')
-const svg2img = require('svg2img')
 const path = require('path')
-const mathJax = require('mathjax-node')
+const svg2img = require('svg2img')
+const { MathJax } = require('mathjax3')
+const { TeX } = require('mathjax3/mathjax3/input/tex')
+const { SVG } = require('mathjax3/mathjax3/output/svg')
+const { RegisterHTMLHandler } = require('mathjax3/mathjax3/handlers/html')
+const { chooseAdaptor } = require('mathjax3/mathjax3/adaptors/chooseAdaptor')
 
 const app = express()
 
-mathJax.start()
+// MathJax bootstrap
+const adaptor = chooseAdaptor()
+RegisterHTMLHandler(adaptor)
 
-function typeset(math, format) {
-  return new Promise((resolve, reject) => {
-    mathJax.typeset(
-      {
-        math,
-        format,
-        svg: true,
-      },
-      function(data) {
-        if (data.errors) {
-          return reject(data.errors[0])
-        }
-        resolve(data.svg)
-      }
-    )
-  })
+const html = MathJax.document('', {
+  InputJax: new TeX(),
+  OutputJax: new SVG({ fontCache: 'none' }),
+})
+
+function tex2svg(equation, isInline = false) {
+  return adaptor.innerHTML(html.convert(equation, { display: !isInline }))
 }
 
-function convertPNG(svgString, args = {}) {
+function svg2png(svgString, args = {}) {
   return new Promise((resolve, reject) => {
     svg2img(svgString, args, function(error, buffer) {
       if (error) {
@@ -47,8 +44,7 @@ app.get('/', async function(req, res, next) {
 
   const isPNG = /\.png$/.test(equation)
   const normalizedEquation = equation.replace(/\.(svg|png)$/, '')
-  const mathJaxFormat = isInline ? 'inline-TeX' : 'TeX'
-  const cacheKey = mathJaxFormat + normalizedEquation
+  const cacheKey = isInline ? 'inline' : 'block' + '.' + normalizedEquation
   const cachedData = memCache.get(cacheKey)
 
   // render equation
@@ -60,11 +56,12 @@ app.get('/', async function(req, res, next) {
   <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
   `)
   }
+
   try {
-    let image = cachedData || (await typeset(normalizedEquation, mathJaxFormat))
+    let image = cachedData || tex2svg(normalizedEquation, isInline)
     if (!cachedData) memCache.put(cacheKey, image, 60 * 1000)
     if (isPNG) {
-      image = new Buffer(await convertPNG(image), 'base64')
+      image = new Buffer(await svg2png(image), 'base64')
     }
     res.end(image)
   } catch (err) {
