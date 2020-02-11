6f973d47e6a8c7a51e0b29217f8f51c1730b46d4
Yasuaki Uechi <y@uechi.io>
2020-02-11 10:16:01 +0900

    chore: fix test

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import helmet from "helmet";
-import express from "express";
-import svg2img from "svg2img";
-import bodyParser from "body-parser";
-import { mathjax } from "mathjax-full/js/mathjax";
-import { TeX } from "mathjax-full/js/input/tex";
-import { SVG } from "mathjax-full/js/output/svg";
-import { LiteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor";
-import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html";
-import { AllPackages } from "mathjax-full/js/input/tex/AllPackages";
+import helmet from 'helmet';
+import express from 'express';
+import svg2img from 'svg2img';
+import bodyParser from 'body-parser';
+import { mathjax } from 'mathjax-full/js/mathjax';
+import { TeX } from 'mathjax-full/js/input/tex';
+import { SVG } from 'mathjax-full/js/output/svg';
+import { LiteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
+import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 
 const app = express();
 
@@ -18,17 +18,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // MathJax bootstrap
 const adaptor = new LiteAdaptor();
 RegisterHTMLHandler(adaptor);
-const html = mathjax.document("", {
+const html = mathjax.document('', {
   InputJax: new TeX({ packages: AllPackages }),
-  OutputJax: new SVG({ fontCache: "none" })
+  OutputJax: new SVG({ fontCache: 'none' }),
 });
 
 function tex2svg(equation: string, isInline: boolean, color: string): string {
   const svg = adaptor
     .innerHTML(html.convert(equation, { display: !isInline }))
     .replace(/fill="currentColor"/, `fill="${color}"`);
-  if (svg.includes("merror")) {
-    return svg.replace(/<rect.+?><\/rect>/, "");
+  if (svg.includes('merror')) {
+    return svg.replace(/<rect.+?><\/rect>/, '');
   }
   return svg;
 }
@@ -42,7 +42,7 @@ function svg2png(svgString: string): Promise<Buffer> {
 
     const args = {
       width: `${width * 3}ex`,
-      height: `${height * 3}ex`
+      height: `${height * 3}ex`,
     };
 
     svg2img(svgString, args, function(error: Error, buffer: Buffer) {
@@ -55,40 +55,40 @@ function svg2png(svgString: string): Promise<Buffer> {
 }
 
 // math parser
-app.get("/", async function(req, res, next) {
-  const mode = Object.keys(req.query).includes("from")
-    ? "block"
-    : Object.keys(req.query).includes("inline")
-    ? "inline"
+app.get('/', async function(req, res, next) {
+  const mode = Object.keys(req.query).includes('from')
+    ? 'block'
+    : Object.keys(req.query).includes('inline')
+    ? 'inline'
     : null;
   if (!mode) {
     return next();
   }
-  const isInline = mode === "inline";
+  const isInline = mode === 'inline';
   const equation = isInline ? req.query.inline : req.query.from;
   if (!equation || equation.match(/\.ico$/)) {
     return next();
   }
 
-  const color = req.query.color || "black";
+  const color = req.query.color || 'black';
   if (/[^a-zA-Z0-9#]/.test(color)) {
     return next();
   }
 
   const isPNG = /\.png$/.test(equation);
-  const normalizedEquation = equation.replace(/\.(svg|png)$/, "");
+  const normalizedEquation = equation.replace(/\.(svg|png)$/, '');
 
   try {
     const svgString = tex2svg(normalizedEquation, isInline, color);
     const imageData = isPNG ? await svg2png(svgString) : svgString;
 
-    res.setHeader("cache-control", "s-maxage=604800, maxage=604800");
+    res.setHeader('cache-control', 's-maxage=604800, maxage=604800');
 
     // render equation
     if (isPNG) {
-      res.contentType("image/png");
+      res.contentType('image/png');
     } else {
-      res.contentType("image/svg+xml");
+      res.contentType('image/svg+xml');
       res.write(`<?xml version="1.0" standalone="no" ?>
   <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
   `);
@@ -96,17 +96,15 @@ app.get("/", async function(req, res, next) {
 
     res.end(imageData);
   } catch (err) {
-    res.write(
-      '<svg xmlns="http://www.w3.org/2000/svg"><text x="0" y="15" font-size="15">'
-    );
+    res.write('<svg xmlns="http://www.w3.org/2000/svg"><text x="0" y="15" font-size="15">');
     res.write(err);
-    res.end("</text></svg>");
+    res.end('</text></svg>');
   }
 });
 
 // welcome page
-app.get("/", function(req, res) {
-  res.redirect(301, "/home");
+app.get('/', function(req, res) {
+  res.redirect(301, '/home');
 });
 
 // app.listen();
--- util/now.ts
+++ util/now.ts
@@ -1,28 +1,30 @@
-import waitOn from "wait-on";
-import getPort from "get-port";
-import spawn from "cross-spawn";
-import request from "supertest";
+import waitOn from 'wait-on';
+import getPort from 'get-port';
+import spawn from 'cross-spawn';
+import request from 'supertest';
+
+const TIMEOUT = 60 * 1000;
 
 export async function launchNow(
-  hostname = "127.0.0.1"
+  hostname = '127.0.0.1',
 ): Promise<[request.SuperTest<request.Test>, () => void]> {
   const port = await getPort();
   const host = `${hostname}:${port}`;
-  const nowDev = spawn("now", ["dev", "--listen", host], {
+  const nowDev = spawn('now', ['dev', '--listen', host], {
     shell: true,
     detached: true,
-    stdio: "ignore"
+    stdio: 'ignore',
   });
   nowDev.unref();
   try {
-    await waitOn({ resources: [`http://${host}`], timeout: 40000 });
+    await waitOn({ resources: [`http://${host}`], timeout: TIMEOUT });
   } catch (err) {
-    throw new Error("Timeout: launching now dev takes too much time");
+    throw new Error('Timeout: launching now dev takes too much time');
   }
   return [
     request(`http://${host}`),
     () => {
-      nowDev.kill("SIGINT");
-    }
+      nowDev.kill('SIGINT');
+    },
   ];
 }
