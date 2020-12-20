ebd5b11942f15c6e23051d4a56edae8e8fac6c3c
Yasuaki Uechi <y@uechi.io>
2020-12-20 14:18:43 +0900

    feat: dark mode support

--- adaptor.ts
+++ adaptor.ts
@@ -1,25 +1,43 @@
-import { mathjax } from 'mathjax-full/js/mathjax';
-import { TeX } from 'mathjax-full/js/input/tex';
-import { SVG } from 'mathjax-full/js/output/svg';
-import { LiteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
-import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
-import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
+import { mathjax } from "mathjax-full/js/mathjax";
+import { TeX } from "mathjax-full/js/input/tex";
+import { SVG } from "mathjax-full/js/output/svg";
+import { LiteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor";
+import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html";
+import { AllPackages } from "mathjax-full/js/input/tex/AllPackages";
 
 // MathJax bootstrap
 const adaptor = new LiteAdaptor();
 RegisterHTMLHandler(adaptor);
 
-const html = mathjax.document('', {
+const html = mathjax.document("", {
   InputJax: new TeX({ packages: AllPackages }),
-  OutputJax: new SVG({ fontCache: 'none' }),
+  OutputJax: new SVG({ fontCache: "none" }),
 });
 
-export function tex2svg(equation: string, isInline: boolean, color: string): string {
+export function tex2svg(
+  equation: string,
+  isInline: boolean,
+  color?: string,
+  alternateColor?: string
+): string {
   const svg = adaptor
     .innerHTML(html.convert(equation, { display: !isInline }))
-    .replace(/fill="currentColor"/, `fill="${color}"`);
-  if (svg.includes('merror')) {
-    return svg.replace(/<rect.+?><\/rect>/, '');
+    .replace(
+      /(?<=<svg.+?>)/,
+      `
+<style>
+  * {
+    fill: ${color || "black"};
+  }
+  @media (prefers-color-scheme: dark) {
+    * {
+      fill: ${alternateColor || color || "white"};
+    }
+  }
+</style>`
+    );
+  if (svg.includes("merror")) {
+    return svg.replace(/<rect.+?><\/rect>/, "");
   }
   return svg;
 }
--- index.ts
+++ index.ts
@@ -1,23 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import {tex2svg} from './adaptor';
+import express from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import { tex2svg } from "./adaptor";
 
 const app = express();
 
 app.use(helmet());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('*', async function (req, res, next) {
-  const mode = Object.keys(req.query).includes('from')
-    ? 'block'
-    : Object.keys(req.query).includes('inline')
-    ? 'inline'
+app.get("*", async function (req, res, next) {
+  const mode = Object.keys(req.query).includes("from")
+    ? "block"
+    : Object.keys(req.query).includes("inline")
+    ? "inline"
     : null;
   if (!mode) {
     return next();
   }
-  const isInline = mode === 'inline';
+  const isInline = mode === "inline";
   const equation = isInline
     ? (req.query.inline as string)
     : (req.query.from as string);
@@ -25,18 +25,27 @@ app.get('*', async function (req, res, next) {
     return next();
   }
 
-  const color = (req.query.color as string) || 'black';
-  if (/[^a-zA-Z0-9#]/.test(color)) {
+  const color = req.query.color as string | undefined;
+  const alternateColor = req.query.alternateColor as string | undefined;
+  if (
+    (color && /[^a-zA-Z0-9#]/.test(color)) ||
+    (alternateColor && /[^a-zA-Z0-9#]/.test(alternateColor))
+  ) {
     return next();
   }
 
-  const normalizedEquation = equation.replace(/\.(svg|png)$/, '');
+  const normalizedEquation = equation.replace(/\.(svg|png)$/, "");
 
   try {
-    const svgString = tex2svg(normalizedEquation, isInline, color);
+    const svgString = tex2svg(
+      normalizedEquation,
+      isInline,
+      color,
+      alternateColor
+    );
 
-    res.setHeader('cache-control', 's-maxage=604800, maxage=604800');
-    res.contentType('image/svg+xml');
+    res.setHeader("cache-control", "s-maxage=604800, maxage=604800");
+    res.contentType("image/svg+xml");
     res.write(`<?xml version="1.0" standalone="no" ?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
 `);
@@ -45,15 +54,15 @@ app.get('*', async function (req, res, next) {
   } catch (err) {
     res.status(500);
     res.write(
-      '<svg xmlns="http://www.w3.org/2000/svg"><text x="0" y="15" font-size="15">',
+      '<svg xmlns="http://www.w3.org/2000/svg"><text x="0" y="15" font-size="15">'
     );
     res.write(err);
-    res.end('</text></svg>');
+    res.end("</text></svg>");
   }
 });
 
-app.get('/', function (req, res) {
-  res.redirect(301, '/home');
+app.get("/", function (req, res) {
+  res.redirect(301, "/home");
 });
 
 export default app;
